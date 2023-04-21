1b1b1471b88df3f0ffc56df2a17b090ec1562f76
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-21 16:40:15 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,7 +9,8 @@
  */
 int main(void)
 {
-        int n;
+        int n:
+
         srand(time(0));
         n = rand() - RAND_MAX / 2;
         if ( n > 0 )
@@ -23,5 +24,6 @@ int main(void)
         else
         {
                 printf("%d is negative \n", n);
-        }
+	}
         return (0);
+}	
