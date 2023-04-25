c3b31760ce1aaf7bf1297ffdaddf72adad206aab
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-25 07:25:32 -0700

    0_putchar

---
+++ 0x02-functions_nested_loops/0_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
+
+int main(void)
+{
+	_putchar('p');
+	_putchar('e');
+	_putchar('\n');
+
+	return (0);
+}
