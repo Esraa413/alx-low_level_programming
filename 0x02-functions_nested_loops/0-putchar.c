8f55f354c3ecd7a800e979128fc6ad4575e323f4
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-25 06:25:09 -0700

    0-putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,12 +4,12 @@
  */
 int main(void)
 {
-	_putchar('_');	
-	_putchar('p');
-	_putchar('u');
+        _putchar('_');
+        _putchar('p');
+        _putchar('u');
 
-	
-	_putchar('\n');
 
-	return (0);
+        _putchar('\n');
+
+        return (0);
 }
