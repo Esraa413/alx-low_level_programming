a2a25c52196f080ca85780cc5e0f919699f2c441
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-25 17:34:14 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,13 +2,13 @@
 
 /**
  * _isalpha- checks for alphabetic character.
- *
+ * @c: the character to be checked
  * Return: 1 if c is a letter, 0 otherwise
  */
 
 int _isalpha(int c)
 {
-	if (c >= 97 && c <= 122)
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 		return (1);
 	}
