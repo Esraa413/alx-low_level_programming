484e10490603b2d6befc8d419f6340454767094d
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-27 12:03:10 -0700

    fizz_buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,32 +10,22 @@
 
 int main(void)
 {
-	int i;
-	
-	for (i = 1; i <= 100; i++)
-	{
-		if (i % 3 == 0 && i % 5 = 0)
-		{
-			printf(" Fizz");
-		}
-		else if (i % 5 == 0 && i % 3 = 0)
-		{
-			printf(" Buzz");
-		}
-		else if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf(" FizzBuzz");
-		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
-		else
-		{
-			printf(" %d", i);
-		}
-		printf("\n");
+	int mun;
 
-		return (0);
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && !(num % 5 == 0))
+			printf("Fizz");
+		else if (num % 5 == 0 && !(num % 3 == 0))
+			printf("Buzz");
+		else if (num % 3 == 0 && num % 5 == 0)
+			printf("Fizz Buzz");
+		else
+			printf("%d", num);
+		if (num != 100)
+		printf(" ");
+		else
+			printf("\n");
 	}
+	return (0);
 }
