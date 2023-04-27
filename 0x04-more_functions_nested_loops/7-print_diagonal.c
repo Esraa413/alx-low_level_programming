63568fa134015c0f11f4edfad3f77e7e201a53c9
Esraa413 <esraaabdelwahab614@gmail.com>
2023-04-27 10:05:48 -0700

    print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,22 +6,25 @@
  */
 
 void print_diagonal(int n)
-
 {
-	int postn, space;
-
-	if (n <= 0 )
+	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		for (postn = 1; postn <= n; postn++)
+		int i, j;
+
+		for (i = 0; i < n; i++)
 		{
-			for (space = 1; space <= space; space++)
-				_putchar(' ');
-			_putchar(92); /*is equal to '/' char*/
-			_putchar('/n')
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
 	}
 }
