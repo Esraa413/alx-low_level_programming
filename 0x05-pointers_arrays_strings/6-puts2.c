132a558e27616e661192de6b8f88799e3b7d55ba
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-02 17:55:54 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string, starting with the first character, followed by a new line
+ * @str: string paraneter input
+ * return (0)
+ */
+
+void puts2(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[x]);
+	}
+	_putchar('\n');
+}
