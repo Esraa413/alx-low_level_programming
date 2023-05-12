8c670c822615e2a2c52720e8642567cd377abd9a
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-12 16:54:01 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,21 +8,20 @@
 
 int _atoi(char *s)
 {
- 	int sign = 1, y = 0;
- 	unsigned int res = 0;
+	int sign = 1, y = 0;
+	unsigned int res = 0;
 
-
- 	while (!(s[y] <= '9' && s[y] >= '0') && s[y] != '\0')
- 	{
- 		if (s[y] == '-')
- 			sign *= -1;
- 		y++;
- 	}
- 	while (s[y] <= '9' && (s[y] >= '0' && s[y] != '\0'))
- 	{
- 		res = (res * 10) + (s[y] - '0');
- 		y++;
- 	}
- 	res *= sign;
- 	return (res);
+	while (!(s[y] <= '9' && s[y] >= '0') && s[y] != '\0')
+	{
+		if (s[y] == '-')
+			sign *= -1;
+		y++;
+	}
+	while (s[y] <= '9' && (s[y] >= '0' && s[y] != '\0'))
+	{
+		res = (res * 10) + (s[y] - '0');
+		y++;
+	}
+	res *= sign;
+	return (res);
 }
