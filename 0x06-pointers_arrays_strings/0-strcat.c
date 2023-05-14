82b93d72dc58299e953a899ea5e77ca936b3fd16
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-14 13:21:53 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
  /**
- * _strcat - concatenates two strings
+ * _strcat -  a function that concatenates two strings.
  * @dest : input value
  * @src : input value
+ *
  * Return: Return: Always 0
  */
 
@@ -12,12 +13,10 @@ char *_strcat(char *dest, char *src)
 	int c, x;
 
 	while (dest[c])
-	{
 		c++;
-	}
+
 	for (x = 0; src[x]; x++)
-	{
 		desr[c++] = src[x];
-	}
+
 	return (dest);
 }
