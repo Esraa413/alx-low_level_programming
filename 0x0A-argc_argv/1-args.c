29ba23c0875fc2afffc8a9fc31c89554889293fa
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-14 16:14:44 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print the new name of the program
+ * @argc: int number of arguments
+ * @argv: list array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char const *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
