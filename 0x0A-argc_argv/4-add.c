0e27b4f35d6c82682b11162a6b7684ece32c7848
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-15 14:12:14 -0700

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - print the new name of the program
+ * @argc: int number of arguments
+ * @argv: list array of arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x = 1;
+	int y;
+	int sum = 0;
+
+	while (x < argc)
+	{
+		if (argv[x])
+		{
+			y = atoi(argv[x]);
+			sum += y;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		x++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
