e12f9f3bdb3b577244ae13e56120a10e5c66c44c
Esraa413 <esraaabdelwahab614@gmail.com>
2023-05-27 08:01:26 -0700

    name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @f: pointers to function
+ * @name: name of the person
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || f)
+		f(name);
+}
