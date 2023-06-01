ce0c71528fbbd16d4d5b1ef126ec4504ab8b8625
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-01 16:24:22 -0700

    node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,15 +14,12 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *i = malloc(sizeof(list_t));
 	int longe = 0;
 
-	i = malloc(sizeof(list_t));
 	if (i == 0)
-	{
+
 		return (0);
-	}
+
 	while (str[longe])
-	{
 		longe++;
-	}
 
 	i->len = longe;
 	i->str = strdup(str);
