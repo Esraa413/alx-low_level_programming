654fecd33e448aa8697be4300961048eafba9b36
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-01 17:10:45 -0700

    end

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - function that frees a list
+ * @head: list to be freed
+ *
+ * Return: Always 0
+ */
+
+void free_list(list_t *head)
+{
+	list_t *tm;
+
+	while (head)
+	{
+		tm = head->next;
+		free(head->str);
+		free(head);
+		head = tm;
+	}
+}
