2ebab7bd41f0828faf33890247886d8a86392f50
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-02 14:20:59 -0700

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,7 +23,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)\n");
 		else
-			printf("%c", str);
+			printf("%s", str);
 		if (sum != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
