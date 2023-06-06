55668fc58f3d3a4ef3d0d5ed9dfead3558670526
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-06 15:12:25 -0700

    insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,7 +9,7 @@
  * @head: pointer to the first node
  * @n: data to insert
  * @idx: index where the new node
- * Returns: the address and NULL if it failed
+ * Return: the address and NULL if it failed
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
