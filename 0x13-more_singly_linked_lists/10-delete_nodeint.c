56e69e369b4973ba4fc46e5ddd4bd7ca68b603b2
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-06 15:08:45 -0700

    delet

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,5 +12,35 @@
 
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *node;
+	listint_t *del_node = NULL;
+	unsigned int x;
 
+	x = 0;
+	node = *head;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		node = *head;
+		*head = (*head)->next;
+		free(node);
+		return (1);
+	}
+
+	while (node)
+	{
+		if (x == index)
+		{
+			del_node->next = node->next;
+			free(node);
+			return (1);
+		}
+		x++;
+		del_node = node;
+		node = node->next;
+	}
+	return (-1);
 }
