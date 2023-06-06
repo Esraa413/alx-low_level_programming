2d5238cfdeb74544b3a1be7ced6cff7f4516c433
Esraa413 <esraaabdelwahab614@gmail.com>
2023-06-06 12:00:35 -0700

    insert

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - deletes the node at index index of a listin
+ * @head: pointer to the first node
+ * @index: index of the node
+ * Returns: 1 if it succeeded, -1 if it failed
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+
+}
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,43 +4,41 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - inserts a new node in a linked list,
- * at a given position
- * @head: pointer to the first node
- * @idx: index where the new node
- * @n: data to insert
- * Returns: the address and NULL if it failed
+ * insert_nodeint_at_index - function that inserts a new node,
+ *  at a given position
+ *  @head: pointer to the first node
+ *  @n: data to insert
+ *  @idx: index where the new node
+ *  Returns: the address and NULL
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+unsigned int x;
 	listint_t *node = malloc(sizeof(listint_t));
-	listint_t *new_node;
-	unsigned int x;
+	listint_t *new_node; 
 
-	if (node == NULL || head == NULL)
+	if (new == null || head == NULL)
 		return (NULL);
 
 	node->n = n;
 	node->next = NULL;
 
-	if (idx == 0)
+	if (idx == NULL)
 	{
 		node->next = *head;
 		*head = node;
 		return (node);
-	}
 	while (head)
-	{
-		if (x == idx - 1)
-		{
+		if (x == idx -1)
 			node->next = new_node->next;
-			new_node->next = node;
+			new_node-|>next = node;
 			return (node);
+			}
+			x++;
+			new_node = new_node->next;
 		}
-		x++;
-		new_node = new_node->next;
-	}
 	free(node);
 	return (NULL);
+
 }
