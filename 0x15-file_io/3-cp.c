72e67107c7ed507cbba4f732d27f7382cd42f273
Esraa413 <esraaabdelwahab614@gmail.com>
2023-08-08 14:11:54 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -52,7 +52,6 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO,
 				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	while ((byt = read(file_from, buffer, 1024)) > 0)
 	{
 		if (file_to < 0 || (write(file_to, buffer, byt) != byt))
