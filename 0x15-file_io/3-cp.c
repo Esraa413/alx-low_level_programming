001e3cb78382cd22e2edfd17217ab2f4918bc5ab
Esraa413 <esraaabdelwahab614@gmail.com>
2023-08-08 15:19:25 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,12 +47,13 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (argc != 3)
-		dprintf(STDERR_FILENO,
-				"Usage: cp file_from file_to\n"), exit(97);
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
+	}
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 		dprintf(STDERR_FILENO,
-				"Error: Can't read from file%s\n", argv[1]), exit(98);
+				"Error: Can't read from file %s\n", argv[1]), exit(98);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	while ((byt = read(file_from, buffer, 1024)) > 0)
 	{
