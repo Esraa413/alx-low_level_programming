cf8f3eea82eff20fe7755aa049b954f871d9f3db
Esraa413 <esraaabdelwahab614@gmail.com>
2023-08-31 16:25:54 -0700

    get_end

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness -  checks the endianness and return the end
+ *
+ * Return: 0
+ */
+
+int get_endianness(void)
+{
+	unsigned int end = 1;
+	char *c;
+
+	c = (char *) &end;
+
+	return (*c);
+}
