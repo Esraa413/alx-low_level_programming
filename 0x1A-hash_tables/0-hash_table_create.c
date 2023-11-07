83777b6268149ecf10527b69928c5ad3d5c528c9
Esraa413 <esraaabdelwahab614@gmail.com>
2023-11-06 17:32:31 -0800

    hash_tabele

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,9 +12,10 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = malloc(sizeof(hash_table_t));
+	hash_table_t *hash_table;
 	unsigned long int x;
 
+	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
 	hash_table->size = size;
