f09c4819948ba1333d60078fa2e720ee719180e5
Esraa413 <esraaabdelwahab614@gmail.com>
2024-02-06 05:39:02 -0800

    serch

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value
+ * @array: array to search
+ * @size: size of array
+ * @value: value to look for
+ *
+ * Return: Always EXIT_SUCCESS
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t x = 1;
+
+	if (!array || size == 0)
+	{
+		return (-1);
+	}
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+			return (x);
+	}
+	return (-1);
+}
