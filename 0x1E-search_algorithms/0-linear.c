ab01012ce448a689f9a74d78fd6492f5ee5aeb20
Esraa413 <esraaabdelwahab614@gmail.com>
2024-02-06 14:24:05 -0800

    search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -20,7 +20,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 
-	for (x = 0; x < size; x++)
+	for (x = 1; x < size; x++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 		if (array[x] == value)
